a40338b07ffb555aa3d710e46629c7b5cce5a3cb
esam faiz <145630584+esamalhiafy@users.noreply.github.com>
2024-02-05 19:16:10 -0800

    Update ee.py

--- ee.py
+++ ee.py
@@ -1,1 +1,15 @@
-print("uhh")
+import cv2
+import matplotlib.pyplot as plt
+
+faceModel='opencv_face_detector_uint8.pb'
+faceconfig='opencv_face_detector.pbtxt'
+
+ageModel='age_net.caffemodel'
+ageConfig='age_deploy.prototxt'
+
+genderModel='gender_net.caffemodel'
+genderConfig='gender_deploy.prototxt'
+
+faceNet=cv2.dnn.readNet(faceModel, faceconfig)
+ageNet=cv2.dnn.readNet(ageModel, ageConfig)
+genderNet=cv2.dnn.readNet(genderModel, genderConfig)
